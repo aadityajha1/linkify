d9d2c87c2f837f156d118b023aa00bed965e8cd2
Aaditya Jha <aadityajha2000@gmail.com>
2024-09-29 19:54:23 +0545

    feat: added user auth middleware, error handler

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,34 +1,42 @@
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { ApolloServer } from "@apollo/server";
 import { NextRequest } from "next/server";
-// import { gql } from "graphql-tag";
 import { resolvers, typeDefs } from "@/graphql/schema";
 import dbConnect from "@/lib/mongodb";
 import { getUser } from "@/utils/auth";
-import { NextApiRequest } from "next";
-
-// const typeDefs = gql`
-//   type Query {
-//     hello: String
-//   }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     hello: () => "Hello world!",
-//   },
-// };
+import mongoose from "mongoose";
 
 const server = new ApolloServer({
   typeDefs: typeDefs,
   resolvers: resolvers,
+  formatError: (formattedError, error) => {
+    console.log("Error in formatError::", formattedError);
+    if (formattedError.extensions?.code === "INTERNAL_SERVER_ERROR") {
+    }
+
+    return {
+      ...formattedError,
+      message: "Something went wrong!",
+    };
+  },
 });
 
 const handler = startServerAndCreateNextHandler<NextRequest>(server, {
   context: async (req) => {
-    await dbConnect();
-    const user = await getUser(req);
-    return { req, user };
+    try {
+      await dbConnect();
+      const user = await getUser(req);
+      return {
+        req,
+        user: {
+          ...user,
+          _id: new mongoose.Types.ObjectId(user._id),
+        },
+      };
+    } catch (err) {
+      console.error("Error in context::", err);
+      return { req: req, user: null };
+    }
   },
 });
 
